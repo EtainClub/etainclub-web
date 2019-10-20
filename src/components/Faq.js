89b2e2e5ee3fee571e0be3850607b8246a98b37f
etain <etainclub@gmail.com>
2019-10-21 01:09:14 +0900

    updated faq page

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -7,12 +7,35 @@ const Faq = () => {
         <div style={styles.container}>
             <h2>{t('Faq.header1.title')}</h2>
             <p>{t('Faq.header1.content1')}</p>
+
             <h2>{t('Faq.header2.title')}</h2>
             <p>{t('Faq.header2.content1')}</p>
+            <p>{t('Faq.header2.content2')}</p>
+            <p>{t('Faq.header2.content3')}</p>
+
             <h2>{t('Faq.header3.title')}</h2>
             <p>{t('Faq.header3.content1')}</p>
+            <p>{t('Faq.header3.content2')}</p>
+            <p>{t('Faq.header3.content3')}</p>
+
             <h2>{t('Faq.header4.title')}</h2>
             <p>{t('Faq.header4.content1')}</p>
+            <p>{t('Faq.header4.content2')}</p>
+            <ul>
+                <li>{t('Faq.header4.list1')}</li>
+                <li>{t('Faq.header4.list2')}</li>
+                <li>{t('Faq.header4.list3')}</li>
+            </ul>
+            <p>{t('Faq.header4.content3')}</p>
+            <p>{t('Faq.header4.content4')}</p>
+            <ul>
+                <li>{t('Faq.header4.list4')}</li>
+                <li>{t('Faq.header4.list5')}</li>
+                <li>{t('Faq.header4.list6')}</li>
+                <li>{t('Faq.header4.list7')}</li>
+                <li>{t('Faq.header4.list8')}</li>
+            </ul>
+
             <h2>{t('Faq.header5.title')}</h2>
             <p>{t('Faq.header5.content1')}</p>
             <p>{t('Faq.header5.content2')}</p>

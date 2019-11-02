df746a1242bae45fe6717007dd3b408c3fabfad2
etain <etainclub@gmail.com>
2019-11-02 12:49:48 +0900

    added a language selection button
    now a user can chnage the language by clicking the button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
 import React, {useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Menu, Button } from 'semantic-ui-react'
+import { Menu, Button, Dropdown } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
+import '../i18n';
+import i18n from '../i18n';
 
 const Header = () => {
     const { t } = useTranslation();
     const [activeItem, setActiveItem] = useState();
+
+    const languageOptions = [
+      { key: 'en', text: 'English', value: 'en' }, 
+      { key: 'ko', text: '한글',     value: 'ko' },
+    ];
+
+    const onLanguageChange = (e, data) => {
+      const lang = data.value;
+      console.log('lang val', lang);
+      i18n.changeLanguage(lang);
+    }
 
     const onMenuClick = (e, { name }) => {
         setActiveItem(name);
@@ -22,51 +35,63 @@ const Header = () => {
         </Menu.Item>
 
         <Menu.Menu position='right'>
-        <Menu.Item
-          as={Link} to='/introduction'
-          name='introduction'
-          active={activeItem === 'introduction'}
-          onClick={onMenuClick}
-        >
-          {t('Header.introduction')}
-        </Menu.Item>
+          <Menu.Item>
+            <Dropdown
+              button
+              className='icon'
+              floating
+              labeled
+              icon='world'
+              options={languageOptions}
+              text={t('Header.language')}
+              onChange={onLanguageChange}
+          />
+          </Menu.Item>
+          <Menu.Item
+            as={Link} to='/introduction'
+            name='introduction'
+            active={activeItem === 'introduction'}
+            onClick={onMenuClick}
+          >
+            {t('Header.introduction')}
+          </Menu.Item>
 
-        <Menu.Item
-          as={Link} to='/howto' 
-          name='howto'
-          active={activeItem === 'howto'}
-          onClick={onMenuClick}
-        >
-          {t('Header.howto')}
-        </Menu.Item>
+          <Menu.Item
+            as={Link} to='/howto' 
+            name='howto'
+            active={activeItem === 'howto'}
+            onClick={onMenuClick}
+          >
+            {t('Header.howto')}
+          </Menu.Item>
 
-        <Menu.Item
-          as={Link} to='/stories'
-          name='stories'
-          active={activeItem === 'stories'}
-          onClick={onMenuClick}
-        >
-            {t('Header.stories')}
-        </Menu.Item>
+          <Menu.Item
+            as={Link} to='/stories'
+            name='stories'
+            active={activeItem === 'stories'}
+            onClick={onMenuClick}
+          >
+              {t('Header.stories')}
+          </Menu.Item>
 
-        <Menu.Item
-          as={Link} to='/faq'
-          name='faq'
-          active={activeItem === 'faq'}
-          onClick={onMenuClick}
-        >
-            {t('Header.faq')}
-        </Menu.Item>
+          <Menu.Item
+            as={Link} to='/faq'
+            name='faq'
+            active={activeItem === 'faq'}
+            onClick={onMenuClick}
+          >
+              {t('Header.faq')}
+          </Menu.Item>
 
-        <Menu.Item
-          as={Link} to='/download'
-        >
-            <Button
-              primary
-            >
-              {t('Header.download')}
-            </Button> 
-        </Menu.Item>
+          <Menu.Item
+            as={Link} to='/download'
+          >
+              <Button
+                primary
+              >
+                {t('Header.download')}
+              </Button> 
+          </Menu.Item>
         </Menu.Menu>
         </Menu>
 
